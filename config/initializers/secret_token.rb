2dc60019637e2457262892f3e875fbc63af7a3c4
Alfredo Suarez <alfredo.s.2014@gmail.com>
2014-01-18 12:08:59 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chawi::Application.config.secret_key_base = 'de8b2d21ada76863b435b8bcf4ba671e27ec9e914065858d4ee857a7822a7917e4b3183da059aa2abd91cc10a7556f8c42651a405da6a40f1de6dd8196c7eaf1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chawi::Application.config.session_store :cookie_store, key: '_chawi_session'
